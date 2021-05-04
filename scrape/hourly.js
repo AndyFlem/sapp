ac94c3aa7458d549d303c8bf88c4aad6a54919f5
Andy Fleming <andy@ulendo.com>
2021-05-04 15:49:35 +0200

    sapp

---
+++ scrape/hourly.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+const { DateTime } = require("luxon")
+
+var pg = require('pg')
+pg.types.setTypeParser(1082, (value) => DateTime.fromISO(value))
+
+const knex = require('knex')({
+  client: 'pg',
+  version: '10',
+  debug:false,
+  connection: {
+    host: '127.0.0.1',
+    user: 'postgres',
+    password: 'extramild20',
+    database: 'sapp'
+  }
+})
+
+
+let rawdata = fs.readFileSync('../raw/IDM-Hourly.json');
+let data = JSON.parse(JSON.parse(rawdata).data)
+console.log(data[0])
+Promise.all(data.map((d,indx) => {
+  if (indx>0){
+    if (d[0].length==18) {
+      const date=DateTime.fromFormat(d[0].split(' ')[0].slice(1,11),'yyyy/LL/dd')
+      const hour=parseInt(d[0].split(' ')[1].slice(0,2))
+      const price = d[1]
+      const turnover = d[2]
+      console.log(d,date.toISODate(),hour, price, turnover)
+      oIns={market: 4, tradedate: date.toISODate(), hour: hour, price: price, volume: turnover}
+      return knex('hourly')
+        .insert(oIns)      
+    }
+  }
+
+})).then(() => {console.log('OKO')})
+
--- scrape/scrape_flows.js
+++ scrape/scrape_flows.js
@@ -2,11 +2,12 @@ const axios = require('axios').default
 const { DateTime } = require("luxon")
 
 var pg = require('pg')
+pg.types.setTypeParser(1082, (value) => DateTime.fromISO(value))
 
 const knex = require('knex')({
   client: 'pg',
   version: '10',
-  debug: true,
+  debug:false,
   connection: {
     host: '127.0.0.1',
     user: 'postgres',
@@ -15,6 +16,7 @@ const knex = require('knex')({
   }
 })
 
+const cats=[5,18,19,22]
 
 getFlows().then(() => {
 
@@ -23,32 +25,50 @@ getFlows().then(() => {
 })
 
 async function getFlows() {
+  let curDate
+  await knex.raw(`SELECT MAX(flowdate) FROM flows`)
+    .then(res => {
+      console.log(res.rows[0].max)
+      curDate = res.rows[0].max
+      curDate=curDate.minus({days: 1}) 
+    })
+    .catch(err => { console.log(err) })
 
-  let curDate = DateTime.fromISO('2016-01-01')
+  console.log(curDate)
 
-  while (curDate < DateTime.fromISO( '2016-01-05')) //'2021-03-31"'))
+  while (curDate <= DateTime.fromISO( '2021-03-31')) //'2021-03-31"'))
   {
     let market = 1
     while (market < 5) {
-      const url=`http://www.sappmarket.com/Home/GetFlow?deliveryDate=${curDate.toFormat('yyyy/LL/dd')}&periodId=0&categoryId=18&marketId=${market}`
+      const url=`http://www.sappmarket.com/Home/GetFlow?deliveryDate=${curDate.toFormat('yyyy/LL/dd')}&periodId=0&categoryId=${cats[market-1]}&marketId=${market}`
       console.log(url)
     
       let response = await axios.get(url)
       //console.log(response.data.excelData)      
     
-      knex('flows')
-        .where({flowdate: curDate.toISODate, market: market})
+      const oDel = {flowdate: curDate.toISODate(), market: market}
+      console.log(oDel) 
+      await knex('flows')
+        .where(oDel)
         .delete()
+        .catch(err => { console.log(err) })
 
-      response.data.excelData.forEach(e => {
-        console.log(e)
-        knex('flows')
-          .insert({flowdate: curDate.toISODate, market: market, flow: e[2], from: e[0],to: e[1]})
-      })
-
-      curDate=curDate.plus({ days: 1 })
+      await Promise.all(
+        response.data.excelData.map(e => {
+          const oIns = {flowdate: curDate.toISODate(), market: market, flow: e[2], from: e[0],to: e[1]}
+          if (e[2]!='Flow Result') {
+            console.log(oIns)
+            return knex('flows')
+              .insert(oIns)
+              .catch(err => { console.log(err) })  
+          } else {
+            return true
+          }
+        })
+      )
       market++
     }
+    curDate=curDate.plus({ days: 1 })
   }
   
   
