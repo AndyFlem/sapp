4da013be31f0e8ccae2d67687a6ed594f73b053a
Andy Fleming <andy@ulendo.com>
2021-05-05 09:26:34 +0200

    update

--- scrape/scrape_flows.js
+++ scrape/scrape_flows.js
@@ -19,45 +19,50 @@ const knex = require('knex')({
 const cats=[5,18,19,22]
 
 getFlows().then(() => {
-
   console.log('OK')
-
 })
 
 async function getFlows() {
   let curDate
   await knex.raw(`SELECT MAX(flowdate) FROM flows`)
     .then(res => {
-      console.log(res.rows[0].max)
-      curDate = res.rows[0].max
-      curDate=curDate.minus({days: 1}) 
+      console.log(res.rows)
+      if (res.rows[0].max) {
+        console.log(res.rows[0].max)
+        curDate = res.rows[0].max
+        curDate=curDate.minus({days: 1})   
+      } else {
+        curDate = DateTime.fromISO( '2016-01-01')
+      }
     })
     .catch(err => { console.log(err) })
 
   console.log(curDate)
 
-  while (curDate <= DateTime.fromISO( '2021-03-31')) //'2021-03-31"'))
+  while (curDate <= DateTime.fromISO( '2021-03-31')) 
   {
     let market = 1
-    while (market < 5) {
-      const url=`http://www.sappmarket.com/Home/GetFlow?deliveryDate=${curDate.toFormat('yyyy/LL/dd')}&periodId=0&categoryId=${cats[market-1]}&marketId=${market}`
+    let period = 1
+
+    const oDel = {flowdate: curDate.toISODate(), market: market}
+    console.log(oDel) 
+    await knex('flows')
+      .where(oDel)
+      .delete()
+      .catch(err => { console.log(err) })    
+
+    while (period < 25) {
+      const url=`http://www.sappmarket.com/Home/GetFlow?deliveryDate=${curDate.toFormat('yyyy/LL/dd')}&periodId=${period}&categoryId=${cats[market-1]}&marketId=${market}`
       console.log(url)
     
       let response = await axios.get(url)
       //console.log(response.data.excelData)      
-    
-      const oDel = {flowdate: curDate.toISODate(), market: market}
-      console.log(oDel) 
-      await knex('flows')
-        .where(oDel)
-        .delete()
-        .catch(err => { console.log(err) })
 
       await Promise.all(
         response.data.excelData.map(e => {
-          const oIns = {flowdate: curDate.toISODate(), market: market, flow: e[2], from: e[0],to: e[1]}
+          const oIns = {flowdate: curDate.toISODate(), market: market, hour: period, flow: e[2], from: e[0],to: e[1]}
           if (e[2]!='Flow Result') {
-            console.log(oIns)
+            // console.log(oIns)
             return knex('flows')
               .insert(oIns)
               .catch(err => { console.log(err) })  
@@ -66,7 +71,7 @@ async function getFlows() {
           }
         })
       )
-      market++
+      period++
     }
     curDate=curDate.plus({ days: 1 })
   }
