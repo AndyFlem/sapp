7ddb8bffb0f4a2c6427784887e62428b812eedb5
Andy Fleming <andy@ulendo.com>
2021-06-01 08:48:00 +0200

    sapp

--- scrape/daily.js
+++ scrape/daily.js
@@ -2,6 +2,9 @@ const fs = require('fs');
 const { DateTime } = require("luxon")
 const axios = require('axios').default
 
+const market = 3
+const cats=[5,18,19,22]
+const prefs=['dam','fpm','fpw']
 
 const base_url=`http://www.sappmarket.com/Home/GetPriceAndTurnOverData`
 
@@ -16,11 +19,11 @@ async function getData() {
 
     while (to <  DateTime.local().endOf('month')) {
     
-        const path = '../months/fpm_' + from.toFormat('yyyy_LL') + '.json'
+        const path = '../input/monthly/' + prefs[market-1] + '_' + from.toFormat('yyyy_LL') + '.json'
 
         if (!fs.existsSync(path) || (to >  DateTime.local().startOf('month'))) {
-            //const url=base_url + `?dateFrom=${from.toFormat('dd/LL/yyyy')}&dateTo=${to.toFormat('dd/LL/yyyy')}&aggPeriodId=1&areaId=0&currencyId=1&marketId=1`
-            const url=base_url + `?dateFrom=${from.toFormat('dd/LL/yyyy')}&dateTo=${to.toFormat('dd/LL/yyyy')}&aggPeriodId=1&areaId=0&currencyId=1&marketId=2`
+            const url=base_url + `?dateFrom=${from.toFormat('dd/LL/yyyy')}&dateTo=${to.toFormat('dd/LL/yyyy')}&aggPeriodId=1&areaId=0&currencyId=1&marketId=` + market
+            
             console.log(url)
             let response = await axios.get(url)
             const jsd = JSON.parse(response.data.data)
