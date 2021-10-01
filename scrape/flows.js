cc3e9aa70a1783abb9d9cd89b6dfc268223da856
Andy Fleming <andy@ulendo.com>
2021-10-01 08:03:57 +0200

    Sept

--- scrape/flows.js
+++ scrape/flows.js
@@ -24,7 +24,7 @@ async function getFlows() {
   console.log(curDate.toISODate())
   let csvData=[]
 
-  while (curDate <= DateTime.fromISO( '2021-09-01')) 
+  while (curDate <= DateTime.fromISO( '2021-10-01')) 
   {
     let period = 1
 
