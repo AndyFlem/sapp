c60d7ee3c86cfe805885a02f7894549f04c6f931
Andy Fleming <andy@ulendo.com>
2023-06-18 13:47:51 +0200

    Update

---
+++ puppet/scrape_flow.js
@@ -0,0 +1,81 @@
+const { DateTime, Interval } = require("luxon")
+const puppeteer = require('puppeteer-extra')
+const StealthPlugin = require('puppeteer-extra-plugin-stealth')
+puppeteer.use(StealthPlugin())
+const fs = require('fs');
+
+const market = 1
+const folder = '../input/raw'
+const cats=[5,18,19,5]
+
+const baseurl = 'https://www.sappmarket.com/Home/GetFlow' // ?deliveryDate=2023/05/01&periodId=13&categoryId=5&marketId=1'
+// const baseurl = 'http://localhost:3000/GetFlow' //?deliveryDate=2023/05/01&periodId=13&categoryId=5&marketId=1'
+
+function PromiseTimeout(delayms) {
+  return new Promise(function (resolve, reject) {
+      setTimeout(resolve, delayms);
+  });
+}
+
+
+startDate = DateTime.fromISO('2023-05-01')
+endDate = DateTime.now().minus({day: 1})
+
+console.log('Market: ', market)
+console.log('Start date: ', startDate.toISODate())
+console.log('End date: ', endDate.toISODate())
+
+
+let requests = []
+const days = Interval.fromDateTimes(startDate, endDate).splitBy({ days: 1 }).map((d) => d.start)
+days.map(v => {
+  const dir = folder + '/' + market + '/' + v.toISODate()
+  if (!fs.existsSync(dir)){
+    fs.mkdirSync(dir);
+  }
+  
+  Array.from({ length: 24 }, (_v, i) => {
+    if (!fs.existsSync(dir + '/' + i + '.json')) {
+      requests.push([Math.random(), v, i + 11])
+    }
+  })
+})
+
+requests.sort((a,b) => a[0]-b[0])
+console.log('Requests: ' + requests.length)
+
+puppeteer.launch({ headless: false }).then(async browser => {
+  
+  let no = 0
+  console.log('Browser')
+  for (const req of requests) {
+    no += 1
+    console.log('No: ', no, ' of: ' , requests.length)
+
+    const page = await browser.newPage()
+  
+    const url = baseurl + `?x=${Math.random()}&deliveryDate=${req[1].toFormat('yyyy/LL/dd')}&periodId=${req[2]}&categoryId=${cats[market-1]}&marketId=${market}`
+    console.log(url)
+
+    await page.goto(url)
+    await page.waitForSelector('pre', {timeout: 300000})
+  
+    const content = await page.content()
+  
+    const pre = await page.$('pre') 
+    const data = JSON.parse(await (await pre.getProperty('textContent')).jsonValue())
+    // console.log(data)
+  
+    const fil = folder + '/' + market + '/' + req[1].toISODate() + '/' + (req[2]-11) + '.json'
+    console.log(fil)
+    fs.writeFileSync(fil, JSON.stringify(data))
+
+    const wait = 200 + Math.random()*500
+    console.log('Waiting: ' + wait)
+    await PromiseTimeout(wait)
+    await page.close()  
+  }  
+
+
+  await browser.close()
+})
--- scrape/flows_new.js
+++ scrape/flows_new.js
@@ -7,6 +7,28 @@ const prefs=['dam','fpm','fpw', 'idm']
 
 const folder = '../input/flows/' + prefs[market-1] + '/'
 
+const config = { headers : {
+  'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+  'Accept-Encoding': 'gzip, deflate, br',
+  'Accept-Language': 'en-GB,en;q=0.5',
+  'Connection': 'keep-alive',
+  'Cache-Control':'no-cache',
+  'Host':'www.sappmarket.com',
+  'Pragma':'no-cache',
+  'DNT':'1',
+  'Sec-Fetch-Dest':'document',
+  'TE':'trailers',
+  'Upgrade-Insecure-Requests':'1',
+  'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/113.0',
+  'Cookie':"aws-waf-token=7b4d7877-45cc-4002-b6f8-5a66cc05497a:CgoAZpVHS54BAAAA:9WSo/OOQ+c4aZp/3IWzPrkKvVB7BaeBoMqWkHtiznuN/W320cBRt7T20mMYjx9iBKL6VW1WhsL7fsuL2D0eLyyAK0gV+0jDFPDLBRj6kgv3tYCq1K2tCX70RBZDMiwUh1IwoFXei+eSpVraRI6Bn/u46V4c+HcCS3m+rsmJuF4IJz27ppgZN5A=="
+}}
+
+function PromiseTimeout(delayms) {
+  return new Promise(function (resolve, reject) {
+      setTimeout(resolve, delayms);
+  });
+}
+
 getFlows().then(() => {
   console.log('OK')
 })
@@ -25,27 +47,34 @@ async function getFlows() {
     if (lastData.length==0) {
       const dte = lastFile.split('.')[0]
       lastDate=dte.split('_')[1] + '-' + dte.split('_')[2] + '-01'
+      lastHour=10
       curDate=DateTime.fromISO(lastDate).plus({month:1})
     } else {
       lastDate=lastData[lastData.length-1].date
+      lastHour=lastData[lastData.length-1].hour + 10
       curDate=DateTime.fromISO(lastDate).plus({day:1})
     }
   }
   console.log("Start date:", curDate.toISODate())
+  console.log("Start hour:", lastHour + 1)
+  
   
   let csvData=[]
 
   while (curDate <= DateTime.now()) 
   {
-    let period = 11
+    let period = lastHour + 1
 
     while (period < 35) {
-      const url=`http://www.sappmarket.com/Home/GetFlow?deliveryDate=${curDate.toFormat('yyyy/LL/dd')}&periodId=${period}&categoryId=${cats[market-1]}&marketId=${market}`
+      const url=`https://www.sappmarket.com/Home/GetFlow?deliveryDate=${curDate.toFormat('yyyy/LL/dd')}&periodId=${period}&categoryId=${cats[market-1]}&marketId=${market}`
       // const url=`http://www.sappmarket.com/Home/GetAreaPrice?deliveryDate=${curDate.toFormat('yyyy/LL/dd')}&periodId=${period}&currencyId=1&marketId=${market}`
+      
+      await PromiseTimeout(5000*Math.random()+ 2000)
       console.log(url)
-    
-      let response = await axios.get(url)
-   
+      let response = await axios.get(url, config)
+     
+      //console.log(response.request)
+      
       response.data.data.forEach(e => {
         if (e.FlowResult>0) {
           const oIns = {date: curDate.toISODate(),  hour: period-11, flow: e.FlowResult,from: e.FromAreaName, to: e.ToAreaName}
@@ -53,14 +82,15 @@ async function getFlows() {
         }
       })
       period++
+      fs.writeFileSync(folder + prefs[market-1] + '_' + curDate.toFormat('yyyy_LL') + '.json', JSON.stringify(csvData))
     }
-    console.log(csvData)
-    fs.writeFileSync(folder + prefs[market-1] + '_' + curDate.toFormat('yyyy_LL') + '.json', JSON.stringify(csvData))
+    // console.log(csvData)
+    
 
     curDate=curDate.plus({ days: 1 })
     if (curDate.day==1) {
       csvData=[]
-    }    
+    }
   } 
   
   return Promise.resolve()
